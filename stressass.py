3c405c91e54c68b2c0435bfe7939ac988c5595a9
gigi007-prog <gigi0okropiridze@gmail.com>
2024-11-21 16:42:49 +0200

    stress levels

--- stressass.py
+++ stressass.py
@@ -1,1 +1,9 @@
 print('my name is Gigi')
+
+#create an input of rate your stress level from 1-10 
+# stress level rates *idea so far*
+# 0-3 low stress levels for this you can give a recommendation something simple like drinking tea watching movie eating favorite food blah blah
+#4-6 medium stress levels that you can suggest a video for meditation or a meditation techmiqu
+#7-10 very high stress level you should make multiple suggestion to as because this is the highest stress/anxiety level so something like breathing technique some othe rmore serious remedies
+
+
