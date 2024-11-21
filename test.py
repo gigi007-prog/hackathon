82b6de1e36d3ac724a66c7e3eebe8bc5ed65f852
Eli <coheneli98@gmail.com>
2024-11-21 15:03:46 +0200

    Created my branch for entertainment recomendations

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Code by eli")
+# print("Code by eli")
+
+def hello(name):
+    '''
+    The function will say hi, it accepts one parameter of name.
+    '''
+    print(f'Hello {name}')
+
+# Calling function with input as parameter
+hello(input('What is your name?'))
