bc2f38d42f11cfdf1c45afbcee76986803dc0611
Eli <coheneli98@gmail.com>
2024-11-21 17:10:11 +0200

    Created basic logic structure

---
+++ entertainment_recomendations.py
@@ -0,0 +1,26 @@
+stress_level = 22
+
+def entertainment_recomendation(stress_level):
+
+    if stress_level < 0 or stress_level > 10:
+        print("Although times can be stressful and make it hard to focus, please input a number between  0 - 10 so we can provide proper help.")
+
+    elif stress_level == 0:
+        pass
+
+    elif stress_level <3:
+        pass
+
+    elif stress_level <5:
+        pass
+
+    elif stress_level <7:
+        pass
+
+    elif stress_level <9:
+        pass
+
+    elif stress_level == 10:
+        pass
+
+entertainment_recomendation(stress_level)
